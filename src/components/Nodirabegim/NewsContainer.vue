e982982f78d60c3921893e4521c87c388b52a176
Nodirabegim <musaevanodirabegim@gmail.com>
2024-12-18 14:08:44 +0500

    created container for news section

--- src/components/Nodirabegim/NewsContainer.vue
+++ src/components/Nodirabegim/NewsContainer.vue
@@ -1,9 +1,20 @@
 <template>
-  <News></News>
+  <div class="news-grid">
+    <News />
+    <News />
+    <News />
+    <News />
+  </div>
 </template>
 
 <script setup>
 import News from "./News.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+.news-grid {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); /* 2 columns, equally spaced */
+  gap: 16px; /* Spacing between grid items */
+}
+</style>
