eb547ac76189f70efb97ed84756d5e2247603cd5
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2024-12-18 11:28:52 +0500

    Project Configuration

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="wrapper">
+    <el-container class="container-layout">
+      <el-header class="sticky-navbar">
+        <el-affix :offset="0">
+          <Header />
+          <MainNavigation />
+        </el-affix>
+      </el-header>
+      <el-container class="layout-content">
+        <el-main class="main-section">
+          <router-view :key="$route.path"></router-view>
+        </el-main>
+      </el-container>
+      <el-footer><Footer /></el-footer>
+    </el-container>
+  </div>
+  <DialogNotification />
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import Footer from "@/components/common/Footer.vue";
+import Header from "@/components/common/Header.vue";
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,32 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import MainLayout from "../layout/MainLayout.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: "/",
+      component: MainLayout,
+      children: [
+        {
+          path: "/home",
+          name: "home",
+          components: {
+            default: () => import("../views/HomeView.vue"),
+          },
+        },
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+
+        {
+          path: "/about",
+          name: "about",
+          component: () => import("../views/AboutView.vue"),
+        },
+      ],
     },
   ],
-})
+});
 
-export default router
+export default router;
