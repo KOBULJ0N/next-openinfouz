67e171abc551c71f208f4d58cfd02d591d10739c
Nodirabegim <musaevanodirabegim@gmail.com>
2024-12-19 17:39:48 +0500

    added Uzbekistan map svg based map

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import Uzbekistan from "@/components/statistics-map/UzbekistanMap.vue";
+</script>
 
 <template>
   <main>
     <h1>Home</h1>
   </main>
+  <Uzbekistan />
 </template>
