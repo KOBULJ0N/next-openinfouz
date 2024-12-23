ee39394cecf6765990f95ccc007dabd8f4583705
Nodirabegim <musaevanodirabegim@gmail.com>
2024-12-23 14:39:10 +0500

    fixed some vue warns

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
-import NodirasTestPage from "../views/NodirasTestPage.vue";
 import MainLayout from "@/layout/MainLayout.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
