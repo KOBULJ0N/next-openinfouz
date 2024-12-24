b285fd48007c4584be494af48e0b42dc522d2abc
KOBULJ0N <qobuljonrasuljonov@gmail.com>
2024-12-24 15:01:50 +0500

    Git merge changes

--- src/shared/components/footer/Footer.vue
+++ src/shared/components/footer/Footer.vue
@@ -1,193 +1,75 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="footer">
-    <div class="footer-wrapper">
-      <div class="footer-left">
-        <h4 class="footer-logo__name" v-html="$t('message.footer_logo_name')"></h4>
-        <a href="https://t.me/openinfouz_bot" target="_blank" class="text-white">
-          <img class="tg-icon" src="../../assets/icons/icons/telegram.svg" alt="tg-icon" />
-          openinfouz_bot
-        </a>
-        <p class="head-info__detail">
-          <a class="text-white" href="tel:+998712317909">
-            <img class="head-icon" src="../../assets/icons/icons/phone-call-white.svg" />
-            +998 71 231 79 09
-          </a>
-        </p>
-        <p>
-          <a
-            class="text-white"
-            href="https://yandex.uz/maps/10335/tashkent/house/YkAYdA9iSEUPQFprfX54dXRqZw==/?ll=69.282108%2C41.294893&z=17"
-          >
-            <img class="head-icon" src="../../assets/icons/icons/map.svg" />
-            {{ $t("message.header_location") }}
-          </a>
-        </p>
+  <footer class="relative bg-cover bg-center text-white py-8 footer-back">
+    <!-- Black overlay -->
+    <div class="absolute inset-0 bg-black bg-opacity-70"></div>
 
-        <p>{{ $t("message.copy_write_footer") }}</p>
-        <p>{{ $t("message.copy_write_footer1") }}</p>
-      </div>
-      <div class="footer-right">
-        <div class="footer-contact-info">
-          <div class="moderator-container">
-            <div class="footer-text-justified">
-              <div class="footer-icon-container">
-                <img src="../../assets/icons/icons/call-icon.svg" class="icon-footer" />
-                <span>{{ $t("message.phone_num_moderator") }}</span>
-              </div>
-              <a class="text-white" href="tel:+712311875">+998 71 231 18 75</a>
-            </div>
-            <div class="footer-text-justified">
-              <div class="footer-icon-container">
-                <img src="../../assets/icons/icons/mailWhite.svg" class="icon-footer" />
-                <span>{{ $t("message.email_moderator") }}</span>
-              </div>
-              <div>
-                <a class="text-white" href="mailto:s.khudayberdiev@napp.uz">info@napp.uz</a>
-                <br />
-              </div>
-            </div>
-            <div class="footer-text-justified">
-              <div class="footer-icon-container">
-                <img src="../../assets/icons/icons/users.svg" class="icon-footer" />
-                <span>{{ $t("message.moderator") }}</span>
-              </div>
-              <span class="right">
-                <a href="https://napp.uz/uz" style="color: #fff" target="_blank">{{ $t("message.napp") }}</a></span
-              >
-            </div>
-          </div>
+    <!-- Content -->
+    <div class="relative px-4 md:px-8 w-screen">
+      <div class="flex flex-col md:flex-row justify-between items-start">
+        <!-- Left Section -->
+        <div class="space-y-4 text-center md:text-left">
+          <h3 class="font-bold text-lg w-full md:w-3/4">
+            ЕДИНЫЙ ПОРТАЛ КОРПОРАТИВНОЙ ИНФОРМАЦИИ НАЦИОНАЛЬНОГО АГЕНТСТВА ПЕРСПЕКТИВНЫХ ПРОЕКТОВ РЕСПУБЛИКИ УЗБЕКИСТАН
+          </h3>
+          <ul class="space-y-2">
+            <li class="flex items-center justify-center md:justify-start">
+              <img class="tg-icon" src="../../assets/icons/old_icons/telegram.svg" alt="tg-icon" />
+              <a href="https://t.me/openinfouz_bot" class="ml-2 hover:underline"> openinfouz_bot </a>
+            </li>
+            <li class="flex items-center justify-center md:justify-start">
+              <img class="head-icon" src="../../assets/icons/old_icons/phone-call-white.svg" />
+              <span class="ml-2">+998 71 231 79 09</span>
+            </li>
+            <li class="flex items-center justify-center md:justify-start">
+              <img class="head-icon" src="../../assets/icons/old_icons/map.svg" />
+              <span class="ml-2">г.Ташкент, Мирабадский район, улица Нукус, 22, 100015</span>
+            </li>
+          </ul>
+        </div>
 
-          <div class="tech-support">
-            <!--      <div class="footer-text-justified">-->
-            <!--        <div class="footer-icon-container">-->
-            <!--          <img  src="/src/assets/images/icons/mailWhite.svg" class="icon-footer">-->
-            <!--          <span>{{ $t("message.email_technician") }}</span>-->
-            <!--        </div>-->
-            <!--        <a class="text-white" href="mailto:admin@fondbozori.uz">admin@fondbozori.uz</a>-->
-            <!--      </div>-->
-            <div class="footer-text-justified">
-              <div class="footer-icon-container">
-                <img src="../../assets/icons/icons/users.svg" class="icon-footer" />
-                <span>{{ $t("message.technical_support") }}</span>
-              </div>
-              <span class="right">
-                <a href="https://www.kapitalbozori.uz/" style="color: #fff">{{
-                  $t("message.technical_supporter")
-                }}</a></span
-              >
-            </div>
-          </div>
+        <!-- Right Section -->
+        <div class="space-y-4 text-center md:text-right mt-8 md:mt-0">
+          <ul class="space-y-2">
+            <li>
+              <span>Телефон модератора: +998 71 231 18 75</span>
+            </li>
+            <li>
+              <span>Электронная почта модератора:</span>
+              <span class="ml-2">info@napp.uz</span>
+            </li>
+            <li>
+              <span>Модератор: Национальное агентство перспективных проектов Республики Узбекистан</span>
+            </li>
+            <li>
+              <span>Техническая поддержка: Информационно-ресурсный центр фондового рынка</span>
+            </li>
+          </ul>
         </div>
       </div>
+
+      <!-- Footer Bottom -->
+      <div class="border-t border-gray-700 mt-8 pt-4 text-center text-sm">
+        <p>При использовании материалов опубликованных на данном сайте, ссылка на openinfo.uz обязательна.</p>
+        <p>Все права защищены © 2015 - 2024</p>
+      </div>
     </div>
-  </div>
+  </footer>
 </template>
 
 <script setup></script>
 
 <style scoped>
-.footer {
+.footer-back {
   background-image: url("../../assets/images/stockmarket.png");
   background-size: cover;
+  background-position: center;
   width: 100%;
+  height: fit-content;
   background-repeat: no-repeat;
-  min-height: 250px;
-  max-height: 1000px;
-}
-.footer-wrapper {
-  display: flex;
-  justify-content: space-between;
-  color: #eeeeee;
-  padding: 20px 60px;
-  height: 100%;
-  gap: 50px;
-}
-
-.footer-left,
-.footer-right {
-  display: flex;
-  flex-direction: column;
-  flex: 1;
-  max-width: 620px;
-  gap: 6px;
-}
-
-.footer-contact-info {
-  display: flex !important;
-  flex-direction: column;
-  gap: 10px;
-}
-
-.moderator-container {
-  padding-bottom: 15px;
-  border-bottom: 1px solid #ffffff73;
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-}
-
-.tech-support {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-}
-
-a {
-  text-decoration: none;
-}
-
-.footer-text-justified {
-  display: flex;
-  gap: 15px;
-  justify-content: space-between;
-}
-
-.footer-icon-container {
-  display: flex;
-  gap: 10px;
-  align-items: center;
-}
-.icon-footer {
-  width: 20px;
-  height: 20px;
 }
 
 .tg-icon {
   width: 28px;
-}
-
-.footer-logo__name {
-  font-size: 1.2rem;
-}
-
-@media only screen and (max-width: 1020px) {
-  .footer-wrapper {
-    flex-direction: column;
-    align-items: center;
-  }
-}
-
-.right {
-  text-align: right;
-  width: 211px;
-}
-
-@media only screen and (max-width: 760px) {
-  .footer-wrapper {
-    flex-direction: column;
-    padding: 20px;
-    text-align: center;
-  }
-
-  .footer-text-justified {
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-  }
-
-  .right {
-    text-align: center;
-    width: auto;
-  }
 }
 </style>
--- src/shared/components/header/Header.vue
+++ src/shared/components/header/Header.vue
@@ -1,49 +1,21 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="Header-Container dark:bg-background-dark">
-    <div class="top-header-component flex gap-2 justify-between items-center">
-      <Logo />
-      <SearchInput />
-      <LanguageSelector @change-lang="changeLang" />
-
-      <ThemeSwitcher v-model:isDark="isDark" />
+  <div class="Header-Container">
+    <TopHeader />
+    <div
+      class="flex items-center justify-center bg-blue-500 text-white text-3xl font-bold"
+    >
+      Tailwind CSS is Working!
     </div>
-    <el-divider></el-divider>
-    <!-- <NavigationComponent /> -->
   </div>
 </template>
 
 <script setup>
-import { i18n } from "@/i18n/translate";
-import { ref, watch } from "vue";
-import Logo from "./Logo.vue";
-import SearchInput from "./SearchInput.vue";
-import LanguageSelector from "./LanguageSelector.vue";
-import ThemeSwitcher from "./ThemeSwitcher.vue";
-
-// const { t } = useI18n();
-const isDark = ref(false);
-const changeLang = lang => {
-  i18n.global.locale.value = lang;
-};
-
-watch(isDark, newValue => {
-  document.documentElement.classList.toggle("dark", newValue); // Apply dark class
-  console.log("isDark", newValue);
-});
+import TopHeader from "./TopHeader.vue";
 </script>
 
 <style scoped>
 .Header-Container {
   background: #0567d1;
-  color: white;
-  padding: 10px 80px;
-}
-
-.lang-icon {
-  width: 26px;
-  height: 26px;
-  border-bottom: 1px solid transparent;
-  cursor: pointer;
 }
 </style>
