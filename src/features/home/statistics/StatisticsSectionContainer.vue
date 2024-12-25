ba1f1e9ae0c119b4f9c722e109e2c66d5b31d5a7
Nodirabegim <musaevanodirabegim@gmail.com>
2024-12-25 11:59:34 +0500

    fixed responsiveness in bigger screens

--- src/features/home/statistics/StatisticsSectionContainer.vue
+++ src/features/home/statistics/StatisticsSectionContainer.vue
@@ -1,10 +1,37 @@
 <template>
+  <div class="filter">
+    <el-row :gutter="20" class="filter-row">
+      <el-col :span="6">
+        <el-select v-model="selectedYear" placeholder="Select Year">
+          <el-option
+            v-for="year in years"
+            :key="year"
+            :label="year"
+            :value="year"
+          />
+        </el-select>
+      </el-col>
+      <el-col :span="6">
+        <el-select v-model="selectedQuarter" placeholder="Select Quarter">
+          <el-option
+            v-for="quarter in quarters"
+            :key="quarter.value"
+            :label="quarter.label"
+            :value="quarter.value"
+          />
+        </el-select>
+      </el-col>
+    </el-row>
+    <el-divider />
+  </div>
+
   <div class="holder">
+    <h2>Statistics</h2>
     <div class="left">
-      <DynamicPieChart :percentage="73" :total="235" />
-      <DynamicPieChart :percentage="35" :total="400" />
-      <DynamicPieChart :percentage="70" :total="522" />
-      <DynamicPieChart :percentage="93" :total="80" />
+      <DynamicPieChart :percentage="73" :total="292" />
+      <DynamicPieChart :percentage="85" :total="450" />
+      <DynamicPieChart :percentage="100" :total="450" />
+      <DynamicPieChart :percentage="96" :total="450" />
     </div>
     <div class="right">
       <UzbekistanMap />
@@ -15,18 +42,49 @@
 <script setup>
 import UzbekistanMap from "../statistics-map/UzbekistanMap.vue";
 import DynamicPieChart from "./DynamicPieChart.vue";
+import { ref } from "vue";
+
+const years = [2023, 2024, 2025];
+const quarters = [
+  { label: "Q1", value: "q1" },
+  { label: "Q2", value: "q2" },
+  { label: "Q3", value: "q3" },
+  { label: "Q4", value: "q4" },
+];
+
+const selectedYear = ref(2024);
+const selectedQuarter = ref("q1");
 </script>
+
 <style>
 .holder {
   display: flex;
   justify-content: space-between;
   gap: 20px;
   margin-top: 20px;
+  max-width: 80%;
+  margin: 0 auto;
 }
 
 .left {
   display: flex;
-
   gap: 20px;
+}
+
+.filter {
+  margin-bottom: 20px;
+  max-width: 80%;
+  margin: 0 auto;
+  margin-top: 40px;
+}
+
+.filter-row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.el-select {
+  width: 100%;
 }
 </style>
